7a69d6ad76eb22fbe2e0e48a0391347f76bf56a0
Vedad Yornic <vedadsa@windowslive.com>
2015-01-06 17:45:39 +0100

    Added homework class, Dynamic string arrays

--- TestingDynamicArrays.java
+++ TestingDynamicArrays.java
@@ -3,22 +3,22 @@ public class TestingDynamicArrays {
 
 	public static void main(String[] args) {
 
-		DynamicArray vedad = new DynamicArray(2);
+		StringDynamicArrays vedad = new StringDynamicArrays(2);
 		
-		vedad.add(1);
-		vedad.add(5);
-		vedad.add(6);
-		vedad.add(12);
-		vedad.add(55);
-		vedad.add(533);
-		vedad.add(1222);
-		vedad.add(17);
+		vedad.add("1");
+		vedad.add("5");
+		vedad.add("6");
+		vedad.add("12");
+		vedad.add("55");
+		vedad.add("533");
+		vedad.add("12222");
+		vedad.add("17");
 		
-		vedad.removeAt(5);
-		vedad.removeAt(3);
-		vedad.removeAt(1);
+		vedad.remove(5);
+		vedad.remove(3);
+		vedad.remove(1);
 		
-		int [] array = vedad.toArray();
+		String [] array = vedad.toArray();
 		
 		for(int i=0; i<array.length;i++){
 			System.out.print(array[i] +" ");
